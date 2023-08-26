ee93f15927d194aaba5d640288b996976f07fc17
Marsel Garifullin <marsel.garifullin@sorvimo.com>
2023-08-27 00:47:15 +0300

    edited carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,20 +1,29 @@
 const track = document.querySelector(".carousel__track");
-const slides = Array.from(track.children);
+let slides = Array.from(track.children);
 const cards = slides.map(slide => slide.children[0]);
 const nextButton = document.querySelector(".carousel__button-right");
 const prevButton = document.querySelector(".carousel__button-left");
 const navDots = document.querySelector(".carousel__nav");
 const dots = Array.from(navDots.children);
+const columnGap = Number(
+    getComputedStyle(document.documentElement)
+    .getPropertyValue('--reviewColumnGap')
+    .slice(0, -2)
+);
 
-const slideWidth = slides[0]?.getBoundingClientRect().width || 500;
+const windowSize = window.innerWidth;
+const isMobile = windowSize < 1296;
+const slideWidth = isMobile ? slides[0].getBoundingClientRect().width 
+    : (slides[0].getBoundingClientRect().width - columnGap) / 2;
 
 const setSlidePosition = (slide, index) => {
-    const gap = 20;
-    slide.style.left = (slideWidth + gap) * index + "px";
+    slide.style.left = (slideWidth + columnGap) * index + "px";
+    slide.style.width = slideWidth + "px";
 };
 
 const moveToSlide = (track, currentSlide, targetSlide) => {
-    track.style.transform = "translateX(-" + targetSlide.style.left + ")";
+    const position = - Number(targetSlide.style.left.slice(0, -2));
+    track.style.transform = "translateX(" + position + "px)";
     currentSlide.classList.remove("current-slide");
     targetSlide.classList.add("current-slide");
 }
@@ -24,13 +33,20 @@ const updateDots = (currentDot, targetDot) => {
     targetDot.classList.add("current-slide");
 }
 
+const updateSlides = () => {
+    slides = Array.from(track.children);
+}
+
+// Set slide position
+slides.forEach(setSlidePosition);
+
 // Set carousel and cards height
+const heights = cards.map(card => card.getBoundingClientRect().height);
 const maxCardHeight = Math.max(...cards.map(card => card.getBoundingClientRect().height));
 const carousel = document.querySelector(".carousel");
 carousel.style.height = maxCardHeight + "px";
 cards.forEach(card => card.style.height = maxCardHeight + "px");
 
-slides.forEach(setSlidePosition);
 
 navDots.addEventListener("click", e => {
     const targetDot = e.target.closest("button");
@@ -48,21 +64,45 @@ navDots.addEventListener("click", e => {
 
 nextButton.addEventListener("click", e => {
     const currentSlide = track.querySelector(".current-slide");
-    const targetSlide = currentSlide.nextElementSibling || slides[0];
+    const targetSlide = currentSlide.nextElementSibling;// || slides[0];
+    if (!targetSlide.nextElementSibling) {
+        updateSlides();
+    
+        const position = Number(slides[slides.length - 1].style.left.slice(0, -2));
+        slides[0].style.left = position + slideWidth + columnGap + "px";
+        
+        const carouselList = document.querySelector(".carousel__track");
+        carouselList.appendChild(carouselList.children[0])
+    }
     moveToSlide(track, currentSlide, targetSlide);
 
     const currentDot = navDots.querySelector(".current-slide");
     const nextDot = currentDot.nextElementSibling || dots[0];
     updateDots(currentDot, nextDot);
+    
 })
 
 prevButton.addEventListener("click", e => {
     const currentSlide = track.querySelector(".current-slide");
     const targetSlide = currentSlide.previousElementSibling || slides[slides.length - 1];
+
+    if (!targetSlide.previousElementSibling) {
+        updateSlides();
+    
+        const position = Number(slides[0].style.left.slice(0, -2));
+        slides[slides.length - 1].style.left = (position - slideWidth - columnGap) + "px";
+        
+        const carouselList = document.querySelector(".carousel__track");
+        carouselList.insertBefore(
+            carouselList.children[carouselList.children.length - 1], 
+            carouselList.children[0]
+        )
+    }
     moveToSlide(track, currentSlide, targetSlide);
 
     const currentDot = navDots.querySelector(".current-slide");
     const prevDot = currentDot.previousElementSibling || dots[dots.length - 1];
     updateDots(currentDot, prevDot);
+
 })
 
